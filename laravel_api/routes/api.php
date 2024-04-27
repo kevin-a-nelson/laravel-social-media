8d1079c77eb3d0a358cde6b71f9ac371beaf01fb
Kevin Nelson <kevinnelson@Kevins-MacBook-Pro.local>
2024-04-27 13:31:15 +0900

    register and login

--- laravel_api/routes/api.php
+++ laravel_api/routes/api.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('contacts', [App\Http\Controllers\ContactController::class, 'contacts']);
 Route::post('save_contact', [App\Http\Controllers\ContactController::class, 'saveContact']);
+Route::get('login', [App\Http\Controllers\UserController::class, 'login']);
+Route::post('user', [App\Http\Controllers\UserController::class, 'create']);
